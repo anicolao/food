0163c94a9468292468d83266652d50de2402bca2
anicolao <anicolao@users.noreply.github.com>
2026-01-12 19:30:49 +0000

    deploy: 2297d71d5423fcd5d6668984fcaa1219fc3be13c

---
+++ pr10/_app/immutable/chunks/CeyonesH.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_11j0ndq?.base??"/food/pr10",a=globalThis.__sveltekit_11j0ndq?.assets??s??"";export{a,s as b};
---
+++ pr10/_app/immutable/entry/start.DQx_hUn8.js
@@ -0,0 +1,1 @@
+import{l as o,a as r}from"../chunks/wZ2q2tVo.js";export{o as load_css,r as start};
