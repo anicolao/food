a2ee70f63fc4dd0b703901e65b7e3617862b42aa
anicolao <anicolao@users.noreply.github.com>
2026-01-12 19:17:22 +0000

    deploy: dbdf230f8faa08793270197cacd2fee2f5f34d6d

---
+++ pr9/_app/immutable/chunks/CFO-mD5Q.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_1p2x1g4?.base??"/food/pr9",a=globalThis.__sveltekit_1p2x1g4?.assets??s??"";export{a,s as b};
---
+++ pr9/_app/immutable/entry/start.CsF-HD-0.js
@@ -0,0 +1,1 @@
+import{l as o,a as r}from"../chunks/DFe7fVdF.js";export{o as load_css,r as start};
