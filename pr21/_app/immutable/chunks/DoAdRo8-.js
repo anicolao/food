611fecb169a9166642d2c1e015d1a6c5f3060ce6
anicolao <anicolao@users.noreply.github.com>
2026-01-13 23:59:05 +0000

    deploy: 37be4df0fb352e57a227ffee0ef2434980e92701

---
+++ pr21/_app/immutable/chunks/DoAdRo8-.js
@@ -0,0 +1,1 @@
+import{v as p,E as t}from"./Cic90rz2.js";import{B as c}from"./B5VU-X0F.js";function E(r,s,...a){var e=new c(r);p(()=>{const n=s()??null;e.ensure(n,n&&(o=>n(o,...a)))},t)}export{E as s};
---
+++ pr21/_app/immutable/chunks/v6Kt9YHn.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_um35j2?.base??"/food/pr21",a=globalThis.__sveltekit_um35j2?.assets??s??"";export{a,s as b};
