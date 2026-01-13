a9abec9e1668f59d49730daf96dd805be15a65bd
anicolao <anicolao@users.noreply.github.com>
2026-01-13 12:59:41 +0000

    deploy: c02f9e8a1ccfa3e1bf6ca814d0818c05cde4c3ee

---
+++ pr14/_app/immutable/chunks/DUMAjmKO.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_1ket09e?.base??"/food/pr14",e=globalThis.__sveltekit_1ket09e?.assets??s??"";export{e as a,s as b};
---
+++ pr14/_app/immutable/entry/start.eLMbZ_QA.js
@@ -0,0 +1,1 @@
+import{l as o,a as r}from"../chunks/ZJyIUW2q.js";export{o as load_css,r as start};
