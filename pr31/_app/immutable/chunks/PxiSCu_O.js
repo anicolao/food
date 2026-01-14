7989931bb7c71387b4fffc19989fed640753e761
anicolao <anicolao@users.noreply.github.com>
2026-01-14 20:39:56 +0000

    deploy: 467638fff35c977a17f02d170e01581a7c20210d

---
+++ pr31/_app/immutable/chunks/PxiSCu_O.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_1j379gz?.base??"/food/pr31",a=globalThis.__sveltekit_1j379gz?.assets??s??"";export{a,s as b};
---
+++ pr31/_app/immutable/entry/start.Dff1wRH5.js
@@ -0,0 +1,1 @@
+import{l as o,b as r}from"../chunks/CSv-6pra.js";export{o as load_css,r as start};
