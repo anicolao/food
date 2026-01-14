90da20a4b5ca848715b435e13ae72d23a33b8744
anicolao <anicolao@users.noreply.github.com>
2026-01-14 14:29:03 +0000

    deploy: 9a23cec7c76b392a28effb71efd36549aaa294ba

---
+++ pr29/_app/immutable/chunks/CGstKS1U.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_yoswan?.base??"/food/pr29",a=globalThis.__sveltekit_yoswan?.assets??s??"";export{a,s as b};
---
+++ pr29/_app/immutable/chunks/DfxSCo7Q.js
@@ -0,0 +1,1 @@
+import{s as e}from"./P80P88fv.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
