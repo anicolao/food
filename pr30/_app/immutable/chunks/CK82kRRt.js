1cc89978e93e3de466d66ef2776a03fafbcd4e56
anicolao <anicolao@users.noreply.github.com>
2026-01-14 19:01:55 +0000

    deploy: 48d628f56f27ae3b0af32137890c463201f4cb78

---
+++ pr30/_app/immutable/chunks/CK82kRRt.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_xafly2?.base??"/food/pr30",a=globalThis.__sveltekit_xafly2?.assets??s??"";export{a,s as b};
---
+++ pr30/_app/immutable/entry/start.BMqDI8U4.js
@@ -0,0 +1,1 @@
+import{l as o,b as r}from"../chunks/Ck2qDS3K.js";export{o as load_css,r as start};
