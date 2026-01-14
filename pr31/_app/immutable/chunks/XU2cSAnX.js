d88455725cdb43cbb551b291b1e3335c2fb3d269
anicolao <anicolao@users.noreply.github.com>
2026-01-14 22:46:01 +0000

    deploy: 52743fed8dcbcfd950a02b0cdf45eb44f0bf75bc

---
+++ pr31/_app/immutable/chunks/XU2cSAnX.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_1qa5la5?.base??"/food/pr31",a=globalThis.__sveltekit_1qa5la5?.assets??s??"";export{a,s as b};
---
+++ pr31/_app/immutable/entry/start.VMEdepF0.js
@@ -0,0 +1,1 @@
+import{l as o,b as r}from"../chunks/Cc1c3oYW.js";export{o as load_css,r as start};
