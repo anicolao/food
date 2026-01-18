b7f933d68a5e2ae4be506fec8fbba6ebce9461e1
Alex Nicolaou <anicolao@gmail.com>
2026-01-17 22:48:41 -0500

    chore: remove chrome channel override to enforce bundled chromium parity

--- playwright.config.ts
+++ playwright.config.ts
@@ -41,7 +41,6 @@ export default defineConfig({
             use: {
                 // Ensure we use the global viewport settings by not overriding them with desktop defaults
                 browserName: 'chromium',
-                channel: 'chrome', // or just use default chromium
             },
         },
     ],
