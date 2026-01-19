a2ecd8d197b37b4fb41ef9d2fb3e2bb6fde9e0c9
anicolao <anicolao@users.noreply.github.com>
2026-01-19 12:36:08 +0000

    deploy: e439622f5c611c229953bc1586dbcee84717daca

---
+++ pr64/_app/immutable/chunks/DRzblWTO.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_1a13o55?.base??"/food/pr64",a=globalThis.__sveltekit_1a13o55?.assets??s??"";export{a,s as b};
---
+++ pr64/_app/immutable/entry/start.D18tz5tC.js
@@ -0,0 +1,1 @@
+import{l as o,c as r}from"../chunks/kebDr3bS.js";export{o as load_css,r as start};
