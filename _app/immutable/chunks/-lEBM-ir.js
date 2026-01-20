cb807157ef0ca86c7d54ef4a066546eb9a3d6b5f
anicolao <anicolao@users.noreply.github.com>
2026-01-20 04:27:08 +0000

    deploy: 0e7c138c5c547e55f5fdd4761ffe2a7c5cddd31e

---
+++ _app/immutable/chunks/-lEBM-ir.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_1gobzf5?.base??"/food",a=globalThis.__sveltekit_1gobzf5?.assets??s??"";export{a,s as b};
---
+++ _app/immutable/entry/start.BAmx7PXz.js
@@ -0,0 +1,1 @@
+import{l as o,c as r}from"../chunks/DXTg7RCQ.js";export{o as load_css,r as start};
