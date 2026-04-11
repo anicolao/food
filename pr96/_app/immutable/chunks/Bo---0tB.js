1941bc02dd49eb5a35ec92b29e58dfec1dca1702
anicolao <anicolao@users.noreply.github.com>
2026-04-11 19:19:23 +0000

    deploy: a748e423869f747cbd8407702761f7b0818976d1

---
+++ pr96/_app/immutable/chunks/Bo---0tB.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_oekpx7?.base??"/food/pr96",e=globalThis.__sveltekit_oekpx7?.assets??s??"";export{e as a,s as b};
---
+++ pr96/_app/immutable/entry/start.DXI1KX0F.js
@@ -0,0 +1,1 @@
+import{l as o,c as r}from"../chunks/C1I_GoPy.js";export{o as load_css,r as start};
