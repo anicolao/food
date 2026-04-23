07befbd7132edfc96cbb6f3f3bcfbcd584509a69
anicolao <anicolao@users.noreply.github.com>
2026-04-23 14:57:27 +0000

    deploy: a7bd20aa283dcf86d9ebe4b2f484c5cb4193cd9c

---
+++ pr113/_app/immutable/chunks/BKGAYMcP.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_1t17vum?.base??"/food/pr113",t=globalThis.__sveltekit_1t17vum?.assets??s??"";export{t as a,s as b};
---
+++ pr113/_app/immutable/entry/start.B9_SDNcS.js
@@ -0,0 +1,1 @@
+import{l as o,c as r}from"../chunks/CHozQg1v.js";export{o as load_css,r as start};
