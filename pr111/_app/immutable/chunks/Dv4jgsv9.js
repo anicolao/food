24dd19b8ad016b431c1906326d058d5e4471baec
anicolao <anicolao@users.noreply.github.com>
2026-04-23 13:45:52 +0000

    deploy: 6bd66943e3b85c3db84333151e43f68d4781e1b4

---
+++ pr111/_app/immutable/chunks/Dv4jgsv9.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_8d0xa6?.base??"/food/pr111",a=globalThis.__sveltekit_8d0xa6?.assets??s??"";export{a,s as b};
---
+++ pr111/_app/immutable/nodes/4.D3dZmi1q.js
@@ -0,0 +1,1 @@
+import"../chunks/DsnmJJEf.js";import"../chunks/CVbfoqoa.js";import{E as t}from"../chunks/Bkz-7hWs.js";function n(o){t(o,{})}export{n as component};
