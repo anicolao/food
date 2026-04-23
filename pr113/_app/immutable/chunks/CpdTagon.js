061c46a7e7028aaf84a719bdfb9ebcbcab3cdd55
anicolao <anicolao@users.noreply.github.com>
2026-04-23 18:17:14 +0000

    deploy: 377a57231116edf3043cdaccc8ccfe5bf1cf37ce

---
+++ pr113/_app/immutable/chunks/CpdTagon.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_1gl0aze?.base??"/food/pr113",a=globalThis.__sveltekit_1gl0aze?.assets??s??"";export{a,s as b};
---
+++ pr113/_app/immutable/entry/start.B5NB5h1n.js
@@ -0,0 +1,1 @@
+import{l as o,c as r}from"../chunks/D02YM7I6.js";export{o as load_css,r as start};
