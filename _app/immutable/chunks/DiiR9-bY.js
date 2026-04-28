a4e466ae52052320eccf3e4d3fd30e8b8fc686ce
anicolao <anicolao@users.noreply.github.com>
2026-04-28 04:38:57 +0000

    deploy: a5a506094d8138789b0b84f3222f3796fec857f2

---
+++ _app/immutable/chunks/DiiR9-bY.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_1wjckzh?.base??"/food",a=globalThis.__sveltekit_1wjckzh?.assets??s??"";export{a,s as b};
---
+++ _app/immutable/nodes/4.dIcnPeJE.js
@@ -0,0 +1,1 @@
+import"../chunks/DsnmJJEf.js";import"../chunks/BS9VtCHV.js";import{E as t}from"../chunks/BQDXMCS6.js";function n(o){t(o,{})}export{n as component};
