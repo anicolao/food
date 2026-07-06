3d072e31bd84fb7297d7014be7e2c6bd4add9925
anicolao <anicolao@users.noreply.github.com>
2026-07-06 06:57:48 +0000

    deploy: a113178e9217acd73574ef51c133bef5600ffa3e

---
+++ pr131/_app/immutable/chunks/DAvDsBii.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_sggdui?.base??"/food/pr131",a=globalThis.__sveltekit_sggdui?.assets??s??"";export{a,s as b};
---
+++ pr131/_app/immutable/nodes/4.DlwWT-Wi.js
@@ -0,0 +1,1 @@
+import"../chunks/DsnmJJEf.js";import"../chunks/BS9VtCHV.js";import{E as t}from"../chunks/BNbl8j5u.js";function n(o){t(o,{})}export{n as component};
