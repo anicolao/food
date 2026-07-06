ab46eb36af2237f758c295617e82219d129ec6d8
anicolao <anicolao@users.noreply.github.com>
2026-07-06 06:37:08 +0000

    deploy: 6ee060769d6c2da60c95074946f46805fc18e2ca

---
+++ pr131/_app/immutable/chunks/ATKafokk.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_wn6pdh?.base??"/food/pr131",a=globalThis.__sveltekit_wn6pdh?.assets??s??"";export{a,s as b};
---
+++ pr131/_app/immutable/entry/start.DLZVvR_5.js
@@ -0,0 +1,1 @@
+import{l as o,c as r}from"../chunks/8Zg9nTLU.js";export{o as load_css,r as start};
