8cee219398466710b4ef648404637983ae8c6277
anicolao <anicolao@users.noreply.github.com>
2026-07-06 07:03:09 +0000

    deploy: 090f8d2d3de058180799cf8d7994c315f68d9119

---
+++ pr131/_app/immutable/chunks/BXKvdQTa.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_qbxbar?.base??"/food/pr131",a=globalThis.__sveltekit_qbxbar?.assets??s??"";export{a,s as b};
---
+++ pr131/_app/immutable/nodes/4.CG8yjqP-.js
@@ -0,0 +1,1 @@
+import"../chunks/DsnmJJEf.js";import"../chunks/BS9VtCHV.js";import{E as t}from"../chunks/F-HKhSp2.js";function n(o){t(o,{})}export{n as component};
